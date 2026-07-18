9da811e02b36deb3fccf85770800c6a05b5d3620
Austin Schey <aschey13@gmail.com>
2026-07-18 14:50:45 -0700

    feat: support egui 0.35 (#107)

--- crates/terminput-egui/src/lib.rs
+++ crates/terminput-egui/src/lib.rs
@@ -3,7 +3,17 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
-#[cfg(any(feature = "egui_0_32", feature = "egui_0_33", feature = "egui_0_34"))]
+#[cfg(any(
+    feature = "egui_0_32",
+    feature = "egui_0_33",
+    feature = "egui_0_34",
+    feature = "egui_0_35"
+))]
 mod mapping;
-#[cfg(any(feature = "egui_0_32", feature = "egui_0_33", feature = "egui_0_34"))]
+#[cfg(any(
+    feature = "egui_0_32",
+    feature = "egui_0_33",
+    feature = "egui_0_34",
+    feature = "egui_0_35"
+))]
 pub use mapping::*;
